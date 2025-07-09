1ce43794ba9ff218c36d11b75a95d1ee0bf0478f
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-09 15:27:12 -0300

    ):

--- src/CursoJava/TResourceBundle/REsourceBundleTest01.java
+++ src/CursoJava/TResourceBundle/REsourceBundleTest01.java
@@ -1,7 +1,12 @@
 package CursoJava.TResourceBundle;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class REsourceBundleTest01 {
     public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle.getBundle("message", new Locale("pt", "BR"));
         
     }
 }
