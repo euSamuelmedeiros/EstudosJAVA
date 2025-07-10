9ee3f533654773228cfcb88c8ff4e49b07d9a248
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-09 23:43:35 -0300

    :)

---
+++ src/CursoJava/Qregex/PatternmatcherTEst01.java
@@ -0,0 +1,14 @@
+package CursoJava.Qregex;
+
+import java.util.regex.Pattern;
+
+public class PatternmatcherTEst01 {
+    public static void main(String[] args) {
+        String regex = "ab";
+        String tex = "abbaababa";
+
+        Pattern pattern = Pattern.compile(regex);
+        
+    }
+    
+}
