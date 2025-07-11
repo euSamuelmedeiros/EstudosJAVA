42fc55b4daaeecdb8842e905c1ddb5986dd5abbb
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-11 12:15:52 -0300

    ;)

--- src/CursoJava/Qregex/PatternmatcherTest02.java
+++ src/CursoJava/Qregex/PatternmatcherTest02.java
@@ -5,8 +5,16 @@ import java.util.regex.Pattern;
 
 public class PatternmatcherTest02 {
     public static void main(String[] args) {
-        String regex = "ab";
-        String tex = "abbabababa";
+
+        //    \\d todos os digitos
+        //     \\D tudo o que nao for digito
+        //     \\s espaços em branco
+        //     \\S todos os caracteres excluindo espaços em branco
+        //     \\w todos os digitos e letras da a-z
+        //     \\W tudo oq nao for digito e letras
+
+        String regex = "\\s";
+        String tex = "AsdaA  LK!@#";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(tex);
---
+++ src/CursoJava/Qregex/PatternmatcherTest03.java
@@ -0,0 +1,30 @@
+package CursoJava.Qregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternmatcherTest03 {
+    public static void main(String[] args) {
+
+        // \\d todos os digitos
+        // \\D tudo o que nao for digito
+        // \\s espaços em branco
+        // \\S todos os caracteres excluindo espaços em branco
+        // \\w todos os digitos e letras da a-z
+        // \\W tudo oq nao for digito e letras
+
+        String regex = "0[xX][0-9a-fA-F]";
+        String tex = "12 0x 0X 0XFFABC 0x109 0x1";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(tex);
+
+        System.out.println(tex);
+        //System.out.println(regex);
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+
+    }
+
+}
