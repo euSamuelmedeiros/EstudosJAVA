e82fa5dad67dc346d3ce5fe7d4bd49a6aa0e6539
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-11 11:01:20 -0300

    ;)

--- src/CursoJava/Qregex/PatternmatcherTest02.java
+++ src/CursoJava/Qregex/PatternmatcherTest02.java
@@ -3,12 +3,14 @@ package CursoJava.Qregex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternmatcherTEst01 {
+public class PatternmatcherTest02 {
     public static void main(String[] args) {
         String regex = "ab";
         String tex = "abbabababa";
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(tex);
+        
         System.out.println(tex);
         System.out.println(regex);
         while (matcher.find()) {
